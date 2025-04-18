fa314f99a40c8139ce8bf9c1cd844c6d22b75629
alexagiv <118969793+alexagiv@users.noreply.github.com>
2025-04-18 15:03:15 +0300

    hw2_q1

--- hw2_q1.py
+++ hw2_q1.py
@@ -17,7 +17,6 @@ MORSE_CODE = {'A': '.-',     'B': '-...',   'C': '-.-.',
               "'": '.----.', '-': '-....-',
               }
 
-
 def english_to_morse(
     input_file: str = "lorem.txt",
     output_file: str = "lorem_morse.txt"
@@ -37,3 +36,26 @@ def english_to_morse(
         Name of output file containing the translated Morse code. Please don't change
         it since it's also hard-coded in the tests file.
     """
+    # Read & uppercase
+    with open(input_file, 'r') as f:
+        text = f.read().upper()
+
+    # Create translation table
+    table = str.maketrans(MORSE_CODE)
+
+    # Translate each word, put each Morse-word on its own line
+    morse = "\n".join(
+        map(
+            lambda line: "\n".join(
+                map(lambda w: w.translate(table), line.split())
+            ),
+            text.splitlines()
+        )
+    )
+
+    # Write out
+    with open(output_file, 'w') as f:
+        f.write(morse)
+
+if __name__ == "__main__":
+    english_to_morse()
